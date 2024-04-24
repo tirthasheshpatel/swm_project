1ef1792a9d848b4d7d08e7b26fd76ccba8b116e2
yashdhamu <ydhamech@asu.edu>
2024-04-24 13:40:39 -0700

    Selenium

---
+++ Selenium/Scraper_Twitter.py
@@ -0,0 +1,124 @@
+
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import re
+
+# Initiate chrome webdriver
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+# Enter twitter URL
+url = 'https://twitter.com/login'
+driver.get(url)
+time.sleep(4)
+
+## Create a text file 'credentials.txt' and add twitter username in the first row and password in the second row
+# Load username from the first row of credentials.txt
+username = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+username.send_keys([line.rstrip('\n') for line in open('credentials.txt')][0])
+
+next_button = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]')
+next_button.click()
+time.sleep(4)
+
+# Load password from the second row of credentials.txt
+password = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+password.send_keys([line.rstrip('\n') for line in open('credentials.txt')][1])
+
+# Locate the login button and sign in to twitter
+login = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div')
+login.click()
+time.sleep(3)
+
+# Locate the search box on the home page and prepare for keyword search queries input
+first_search = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input')
+first_search.send_keys('CNN Breaking News since:2019-10-03 until:2019-10-10')
+first_search.send_keys(Keys.ENTER)
+time.sleep(2)
+
+# SECTION B - TWEET LOADING AND SCRAPING
+
+# Use regex to extract tweet features including username, userhandle, post date and tweet text
+user_name = re.compile(r'.+?@')
+user_handle = re.compile(r'@.+?·')
+post_date = re.compile(r'·[A-z][a-z][a-z].+?, [0-9][0-9][0-9][0-9]')
+tweet_text = re.compile(r', [0-9][0-9][0-9][0-9].+')
+
+# Empty dictionary to store tweet features
+twitter_data = {}
+tweet_count = 0
+
+# Iterating over the list of queries generated from Twitter_Query_Generator.py
+while tweet_count < 100:
+    start_scrape_time = time.monotonic()
+    source = driver.page_source
+    soup = BeautifulSoup(source, 'html.parser')
+    tweet_data = soup.find_all('article', {'aria-haspopup': 'false'})
+    tweet_data = soup.find_all('article', {'role': 'article'})
+
+    tweet_dup = []
+
+    for tweet_du in tweet_data:
+        tweet_dup.append(tweet_du)
+
+    for new_article in tweet_data:
+        all_text = new_article.text
+        try:
+            user = re.findall(user_name, all_text)[0]
+            user = user[:-1]
+        except:
+            user = 'N/A'
+        try:
+            handle = re.findall(user_handle, all_text)[0]
+            handle = handle[:-1]
+            replace = re.sub("@", "/", handle)
+            true_handle = 'https://twitter.com' + replace
+        except:
+            true_handle = 'N/A'
+        try:
+            date = re.findall(post_date, all_text)[0]
+        except:
+            date = 'N/A'
+
+        try:
+            tweet_find = re.findall(tweet_text, all_text)[0]
+            tweet_http = re.sub(', [0-9][0-9][0-9][0-9]', '', tweet_find)
+            tweet = re.sub(
+                'https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}',
+                '', tweet_http)
+        except:
+            tweet = 'N/A'
+
+        tweet_count += 1
+        end_scrape_time = time.monotonic()
+        duration = end_scrape_time - start_scrape_time
+        twitter_data[tweet_count] = [user, true_handle, date, tweet, duration]
+
+    # Tweets do not load unless page is scrolled down. To counter this issue 'JavaScript injection' was used
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    driver.execute_script("window.scrollBy(0, -2)")
+    driver.execute_script("window.scrollBy(0, 3)")
+    driver.execute_script("window.scrollBy(0, -2)")
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+# Save scraped data to a csv file
+try:
+    twitter_df = pd.DataFrame.from_dict(twitter_data, orient='index',
+                                        columns=['Username', 'Handle', 'Published', 'Tweet', 'Duration'])
+    twitter_df['Username'] = twitter_df['Username'].map(
+        lambda x: x.encode('unicode-escape').decode('utf-8'))
+    twitter_df['Handle'] = twitter_df['Handle'].map(
+        lambda x: x.encode('unicode-escape').decode('utf-8'))
+    twitter_df['Published'] = twitter_df['Published'].map(
+        lambda x: x.encode('unicode-escape').decode('utf-8'))
+    twitter_df['Tweet'] = twitter_df['Tweet'].map(
+        lambda x: x.encode('unicode-escape').decode('utf-8'))
+    twitter_df.to_csv('CNN_tweets.csv', index=False)  # Rename the file here
+except Exception as e:
+    print(e)
---
+++ Selenium/url_scraper.py
@@ -0,0 +1,72 @@
+# Selenium libraries
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium import webdriver
+
+# Other libraries
+import os
+import time
+import datetime
+import pandas as pd
+import numpy as np
+
+start_date = datetime.datetime.strptime("01-01-2018", "%d-%m-%Y")  # start date query
+end_date = datetime.datetime.strptime("01-01-2019", "%d-%m-%Y")  # end date query
+
+date_range = pd.date_range(start_date, end_date)  # Create date range
+
+# Generate URLs for each date
+urls = [
+    f"https://www.nytimes.com/search?dropmab=false&endDate={date.strftime('%Y-%m-%d')}&query=&sections=U.S.%7Cnyt%3A%2F%2Fsection%2Fa34d3d6c-c77f-5931-b951-241b4e28681c&sort=best&startDate={date.strftime('%Y-%m-%d')}&types=article"
+    for date in date_range
+]
+
+# Open the browser outside the loop
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+browser = webdriver.Chrome(options=chrome_options)
+# browser.maximize_window()
+links = pd.DataFrame(columns=["url", "text", "date"])
+
+def scroll_page():
+    try:
+        while True:
+            button_element = browser.find_element(By.XPATH, '//*[@id="site-content"]/div/div[2]/div[2]/div/button')
+            if button_element:
+                button_element.click()  # Click button to load more content
+                time.sleep(np.random.uniform(2, 6))  # Wait for content to load
+    except NoSuchElementException:
+        pass
+
+def collect(date):
+    try:
+        link_element = browser.find_element(By.XPATH, '//*[@id="site-content"]/div/div[2]/div/ol/li[1]/div/div/div/a')
+        link_text = link_element.text
+        if "PRINT EDITION" in link_text:
+            url = link_element.get_attribute("href")
+            links.loc[len(links)] = [url, link_text, date]
+    except NoSuchElementException:
+        pass
+
+print("Starting Time:", datetime.datetime.now())
+for i, url in enumerate(urls):
+    try:
+        browser.get(url)
+        scroll_page()
+        collect(date_range[i].strftime('%Y-%m-%d'))
+        if i % 10 == 0:
+            break
+        print(f"{i+1} out of {len(urls)}", end="\r")
+    except Exception as e:
+        print(f"Error processing URL {i+1}: {e}")
+
+# Close the browser after processing all URLs
+browser.quit()
+
+links.to_csv("nyt_urls_2018.csv", index=False)
+print("End Time:", datetime.datetime.now())
+os.system("echo 'I am done collecting urls'")
